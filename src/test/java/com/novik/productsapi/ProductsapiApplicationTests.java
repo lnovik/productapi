8fc2a1b42ff52864225fd3b951a90ba67dd67ee2
Luan Novik <luannoviksilva@gmail.com>
2021-11-16 20:18:37 -0300

    create project

---
+++ src/test/java/com/novik/productsapi/ProductsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novik.productsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
