008c70710fd2e8bf15b70dfdd102e5f097f030aa
Luan Novik <luannoviksilva@gmail.com>
2021-11-20 03:11:57 -0300

    Developed update by id feature

--- src/main/java/com/novik/productsapi/service/ProductService.java
+++ src/main/java/com/novik/productsapi/service/ProductService.java
@@ -28,9 +28,7 @@ public class ProductService {
     public MessageResponseDTO createProduct(ProductDTO productDTO) {
         Product productToSave = productMapper.toModel(productDTO);
         Product savedProduct = productRepository.save(productToSave);
-        return MessageResponseDTO.builder()
-                .message("Product created with ID: " + savedProduct.getId())
-                .build();
+        return createMessageResponse(savedProduct.getId(), "Product update with ID: ");
     }
 
     public List<ProductDTO> listAll() {
@@ -40,21 +38,33 @@ public class ProductService {
                 .collect(Collectors.toList());
     }
 
-
     public ProductDTO findById(Long id) throws ProductNotFoundException {
         Product product = verifyIfExists(id);
         return productMapper.toDTO(product);
     }
 
-    public void delete(Long id) throws ProductNotFoundException {
+    public void deleteById(Long id) throws ProductNotFoundException {
         verifyIfExists(id);
         productRepository.deleteById(id);
     }
+    public MessageResponseDTO updateById(Long id, ProductDTO productDTO) throws ProductNotFoundException {
+        verifyIfExists(id);
+        Product productToSave = productMapper.toModel(productDTO);
+        Product updateProduct = productRepository.save(productToSave);
+        return createMessageResponse(updateProduct.getId(), " Product update with ID: ");
+    }
 
+    private MessageResponseDTO createMessageResponse(Long id, String message){
+        return MessageResponseDTO
+                .builder()
+                .message(message + id)
+                .build();
+    }
     private Product verifyIfExists(Long id) throws ProductNotFoundException {
         return productRepository.findById(id)
                 .orElseThrow(() -> new ProductNotFoundException(id));
     }
+
 
 }
 
